3bccbeb2a79214544d7a41e5a9bd8410cdee1fde
buck heroux <bheroux@palantir.com>
2016-07-08 16:52:27 -0400

    cert endpoint

---
+++ registry/mint/jwk.go
@@ -0,0 +1,49 @@
+package mint
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	pemlib "encoding/pem"
+	"fmt"
+
+	"gopkg.in/square/go-jose.v1"
+)
+
+func LoadECPrivateKey(pem []byte) (pk *ecdsa.PrivateKey, err error) {
+	k, err := jose.LoadPrivateKey(pem)
+	if err != nil {
+		return
+	}
+	pk, ok := k.(*ecdsa.PrivateKey)
+	if !ok {
+		err = fmt.Errorf("Key is not ecdsa")
+	}
+	return
+}
+
+func GenES256Key() (pem []byte, err error) {
+	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return
+	}
+	der, err := x509.MarshalECPrivateKey(key)
+	if err != nil {
+		return
+	}
+	buf := bytes.NewBuffer(pem)
+	err = pemlib.Encode(buf, &pemlib.Block{Type: "EC PRIVATE KEY", Bytes: der})
+	return
+}
+
+func MarshalJwkJSON(kid, alg string, key interface{}) ([]byte, error) {
+	jwk := jose.JsonWebKey{
+		Key:       key,
+		KeyID:     kid,
+		Algorithm: string(jose.ES256),
+		Use:       "sig",
+	}
+	return jwk.MarshalJSON()
+}
